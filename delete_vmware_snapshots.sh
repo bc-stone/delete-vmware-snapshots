29e0414df80ef702e4f0c73a0456c0fe81b64b96
Brad Stone <brad.stone.git@gmail.com>
2023-04-06 11:24:17 -0400

    Updated syntax

--- delete_vmware_snapshots.sh
+++ delete_vmware_snapshots.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
-
+#!/bin/bash
+#Example script for use with cron
 source /home/<CHANGEME>/.bashrc
 /usr/bin/nohup /<PATH_TO_PYTHON>/bin/python /<PATH_TO_SCRIPT>/delete_vmware_snapshots.py &
